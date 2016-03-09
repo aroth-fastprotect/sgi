f092213b192ae344375c76f0a43bdfee858826e1
Andreas Roth <andreas.roth@fastprotect.net>
2016-03-09 09:23:45 +0100

    only get real instance of a proxy when when someone needs it

--- include/sgi/plugins/SGIProxyItem.h
+++ include/sgi/plugins/SGIProxyItem.h
@@ -12,27 +12,16 @@ class SGIPluginHostInterface;
 class SGIProxyItemBase : public SGIItemBase
 {
 public:
-    SGIProxyItemBase(SGIPluginHostInterface * hostInterface=NULL, SGIItemBase * realItem=NULL)
-        : _hostInterface(hostInterface), _realItem(realItem) {}
-    SGIProxyItemBase(SGIPluginHostInterface * hostInterface, const std::string & name, const std::string & typeName=std::string(), const std::string & displayName=std::string(), SGIItemBase * realItem=NULL)
-        : _hostInterface(hostInterface), _name(name), _displayName(displayName), _typeName(typeName)
-        , _realItem(realItem) {}
-    SGIProxyItemBase(const SGIProxyItemBase & rhs)
-        : _hostInterface(rhs._hostInterface), _name(rhs._name), _displayName(rhs._displayName)
-        , _typeName(rhs._typeName), _realItem(rhs._realItem) {}
+    SGIProxyItemBase(SGIPluginHostInterface * hostInterface = NULL, SGIItemBase * realItem = NULL);
+    SGIProxyItemBase(SGIPluginHostInterface * hostInterface, 
+        const std::string & name, 
+        const std::string & typeName = std::string(), 
+        const std::string & displayName = std::string(), 
+        SGIItemBase * realItem = NULL);
+    SGIProxyItemBase(const SGIProxyItemBase & rhs);
 
-    SGIItemBase * realItem(bool getInstance=false)
-    {
-        if(!_realItem.valid())
-            _realItem = getRealInstance();
-        return _realItem;
-    }
-    const SGIItemBase * realItem(bool getInstance=false) const
-    {
-        if(!_realItem.valid())
-            const_cast<SGIProxyItemBase*>(this)->_realItem = const_cast<SGIProxyItemBase*>(this)->getRealInstance();
-        return _realItem;
-    }
+    SGIItemBase * realItem(bool getInstance = false);
+    const SGIItemBase * realItem(bool getInstance = false) const;
 
     template<typename ITEMTYPE>
     ITEMTYPE * realItemAs(bool getInstance=false)
