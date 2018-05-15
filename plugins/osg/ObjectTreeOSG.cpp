642617c6b02040e1d9d69719ca92ca49d24a6a77
Andreas Roth <andreas.roth@fastprotect.net>
2018-05-15 14:46:30 +0200

    remove duplicated "Indicies" entry

--- plugins/osg/ObjectTreeOSG.cpp
+++ plugins/osg/ObjectTreeOSG.cpp
@@ -1967,13 +1967,7 @@ bool objectTreeBuildImpl<osg::DrawElements>::build(IObjectTreeItem * treeItem)
         ret = callNextHandler(treeItem);
         if(ret)
         {
-            unsigned numIndices = object->getNumIndices();
-            if(numIndices)
-                treeItem->addChild(helpers::str_plus_count("Indices", numIndices), cloneItem<SGIItemOsg>(SGIItemTypeDrawElementsIndicies));
         }
-        break;
-    case SGIItemTypeDrawElementsIndicies:
-        ret = true;
         break;
     default:
         ret = callNextHandler(treeItem);
