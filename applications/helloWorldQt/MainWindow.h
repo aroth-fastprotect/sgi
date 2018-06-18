054046b03e0f2ec64dc1dc30842bc99b9d377cc3
Andreas Roth <aroth@arsoft-online.com>
2018-06-18 18:47:14 +0200

    add button/menu to show SGI version info create simple functions to communicate with SGI loader qt image plugin

--- applications/helloWorldQt/MainWindow.h
+++ applications/helloWorldQt/MainWindow.h
@@ -11,11 +11,12 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-                        MainWindow(QImage * image, QWidget * parent=NULL);
+                        MainWindow(QImage * image, QWidget * parent=nullptr);
     virtual             ~MainWindow();
 
 public slots:
     void                fileQuit();
+    void                sgiVersionInfo();
 
 private:
     Ui::MainWindow *   ui;
